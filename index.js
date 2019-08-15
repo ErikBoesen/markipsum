31724aaefaeac6e0cf100c7a51799655020af186
ErikBoesen <me@erikboesen.com>
2019-08-15 13:27:48 -0400

    Add sentence generation

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ exports.word = function() {
     return words[Math.floor(Math.random() * words.length)];
 };
 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 exports.sentence = function() {
-    console.log(exports.word());
+    let count = Math.floor(Math.random() * 9);
+    // Add root word
+    let string = capitalize(exports.word());
+    for (let word = 0; word < count; word++) {
+        if (Math.random() < 0.2) {
+            string += ","
+        }
+        string += " " + exports.word();
+    }
+    string += punctuation();
+    return string;
 };
+
+console.log(exports.sentence());
